8fc59badbadfdb8c9bf8117c9b0992c43e2b751c
Vlad <20vladislaw04gmail.com>
2023-02-15 14:57:50 +0200

    закончил работу

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,6 +1,6 @@
 export let movieList = null
 export let inputSearch = null
-export let triggerMode = false 
+export let triggerMode = false
 
 const createElement = ({
   type,
@@ -12,15 +12,13 @@ const createElement = ({
 }) => {
   const el = document.createElement(type)
   Object.keys(attrs).forEach((key) => {
-
-
     if (key !== 'innerHTML') el.setAttribute(key, attrs[key])
     else el.innerHTML = attrs[key];
   });
 
   if (container && position === 'append') container.append(el)
   if (container && position === 'prepend') container.prepend(el)
-  if(evt && handler && typeof handler === 'function') el.addEventListener(evt, handler)
+  if (evt && handler && typeof handler === 'function') el.addEventListener(evt, handler)
   return el;
 }
 
@@ -30,80 +28,101 @@ export const createStyle = () => {
     type: 'style',
     attrs: {
       innerHTML: `
-      body {
-        background-color: blueviolet;
-      }
-      
       * {
-        -webkit-box-sizing: border-box;
-                box-sizing: border-box;
-      }
+      -webkit-box-sizing: border-box;
+      box-sizing: border-box;
+    }
+
+    h1 {
+      text-align: center;
+      z-index: 10;
+      position: relative;
+    }
+    
+    body {
       
-      body {
-        margin: 0;
-        background-color: blueviolet;
-      }
-      
-      .container {
-        padding: 20px;
-      }
-      
-      .movies {
-        display: grid;
-        -webkit-box-pack: center;
-            -ms-flex-pack: center;
-                justify-content: center;
-        gap: 20px;
-      }
-      
-      .movie {
-        display: -webkit-box;
-        display: -ms-flexbox;
-        display: flex;
-        -webkit-box-pack: center;
-            -ms-flex-pack: center;
-                justify-content: center;
-        -webkit-box-align: center;
-            -ms-flex-align: center;
-                align-items: center;
-      }
-      
-      .movie__image {
-        width: 100%;
-        -o-object-fit: cover;
-           object-fit: cover;
-      }
-      
-      .search {
-        margin-bottom: 30px;
-      }
-      
-      .search__label-input {
-        display: block;
-        margin-bottom: 30px;
-      }
-      
-      .search__input {
-        background-color: blueviolet;
-        display: block;
-        max-width: 400px;
-        width: 100%;
-        padding: 10px 15px;
-        margin-bottom: 10px;
-        border: 1px solid black;
-        border-radius: 4px;
-      }
-      
-      .search__label-checkbox {
-        display: inline-block;
+      margin: 0;
+      background-color: rgb(59, 56, 56);
+      overflow-y: scroll;
+    }
+    
+    .container {
+      padding: 0 20px;
+      max-width: 1340px;
+      margin: 0 auto;
+      border: 1px solid black;
+      box-shadow: 0 0 20px white;
+    }
+    
+    .movies {
+      display: grid;
+      -webkit-box-pack: center;
+      -ms-flex-pack: center;
+      justify-content: center;
+      grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+      gap: 20px;
+    }
+    
+    .movie {
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-box-pack: center;
+      -ms-flex-pack: center;
+      justify-content: center;
+      -webkit-box-align: center;
+      -ms-flex-align: center;
+      align-items: center;
+    }
+    
+    .movie__image {
+      width: 100%;
+      -o-object-fit: cover;
+      object-fit: cover;
+    }
+    
+    .search {
+      margin-bottom: 30px;
+      display: grid;
+    }
+    
+    .search__label-input {
+      color: #df5e14;
+      display: block;
+      margin-bottom: 15px;
+      transform: translate(-20px, 3px);
+      font-size: 24px;
+      font-weight: 700;
+    }
+    
+    .search__input {
+      background-color: lightgray;
+      max-width: 400px;
+      width: 100%;
+      padding: 10px 15px;
+      margin-bottom: 10px;
+      border: 3px solid black;
+      border-radius: 4px;
+    }
+    
+    .search__label-checkbox {
       transform: translate(7px, -2px);
-      }
+    }
+    
+    .search__group-checkbox {
+      display: flex;
+    }
+    
+    .search__group {
+      display: flex;
+      justify-content: end;
+      margin-top: 30px;
+      margin-right: 50px;
+    }
       `
     },
     container: document.head
   })
-
-
 }
 
 export const createMarkup = () => {
@@ -132,21 +151,22 @@ export const createMarkup = () => {
     attrs: { class: 'search__group search__group--input' },
     container: searchBox
   })
+
   const checkBox = createElement({
     type: 'div',
     attrs: { class: 'search__group search__group--checkbox' },
     container: searchBox
   })
+
   createElement({
     type: 'label',
     attrs: {
       class: 'search__label-input',
       for: 'search',
-      innerHTML: 'Поиск фильмов '
+      innerHTML: 'Поиск фильмов:'
     },
     container: inputBox
   })
-
 
   inputSearch = createElement({
     type: 'input',
@@ -158,6 +178,7 @@ export const createMarkup = () => {
     },
     container: inputBox
   })
+  
   createElement({
     type: 'input',
     attrs: {
@@ -211,6 +232,3 @@ export const addMovieToList = (movie) => {
 export const clearMoviesMarkup = (el) => {
   el && (el.innerHTML = '')
 }
-
-
-
--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,14 +1,19 @@
-import { addMovieToList, createMarkup, createStyle, movieList, inputSearch, triggerMode, clearMoviesMarkup } from "./dom.js";
+import {
+  addMovieToList,
+  createMarkup,
+  createStyle,
+  movieList,
+  inputSearch,
+  triggerMode,
+  clearMoviesMarkup
+} from "./dom.js";
 
 const getData = (url) => fetch(url)
   .then((res) => res.json())
   .then((data) => data.Search)
-
-
 let searchLast = null
 
 const debounce = (() => {
-
   let timer = null
 
   return (cb, ms) => {
@@ -19,18 +24,19 @@ const debounce = (() => {
     timer = setTimeout(cb, ms)
   }
 })()
+
 const inputSearchHandler = (e) => {
   debounce(() => {
     const searchSrting = e.target.value.trim();
 
     if (searchSrting && searchSrting.length > 3 && searchLast !== searchSrting) {
-      if(!triggerMode) clearMoviesMarkup(movieList)
-      
+      if (!triggerMode) clearMoviesMarkup(movieList)
+
       getData(`http://www.omdbapi.com/?apikey=303ac054&s=${searchSrting}`)
         .then((movies) => movies.forEach((movie => addMovieToList(movie))))
         .catch(console.log)
     }
-    searchLast = searchSrting 
+    searchLast = searchSrting
   }, 2000)
 }
 
